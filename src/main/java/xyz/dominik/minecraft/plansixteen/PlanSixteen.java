c000be85d3ebedf5db8955c9c408bd79ccff434f
TimothyGillespie <Timothy.Gillespie@outlook.de>
2021-06-02 00:17:59 +0200

    Changes to make first working version

--- src/main/java/xyz/dominik/minecraft/plansixteen/PlanSixteen.java
+++ src/main/java/xyz/dominik/minecraft/plansixteen/PlanSixteen.java
@@ -3,15 +3,21 @@ package xyz.dominik.minecraft.plansixteen;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Objects;
+
 public final class PlanSixteen extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        Bukkit.getPluginCommand("heal").setExecutor(new HealCommand());
+        this.register();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    protected void register() {
+        Bukkit.getPluginCommand("heal").setExecutor(new HealCommand());
     }
 }
